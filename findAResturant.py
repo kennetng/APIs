933010ba89648a246d33553cb96255187fbcb145
Kenneth Ngo <>
2018-09-05 17:39:20 +0200

    Getting the picture url from the first restaurant

--- findAResturant.py
+++ findAResturant.py
@@ -19,22 +19,33 @@ def findARestaurant(mealType,location):
 	url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s'% (foursquare_client_id, foursquare_client_secret, latitude, longitude, mealType))
 	h = httplib2.Http()
 	result = json.loads(h.request(url,'GET')[1])
-	#Get the first resturant
+
+	#Find the first resturant
 	firstResturant = result['response']['venues'][0]
-	firstResturant_id = firstResturant['id']
 	
+	#Get the venue id of the resutrant so we can retrieve the photo of it
+	venue_id = firstResturant['id']
+	url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % (venue_id, foursquare_client_id, foursquare_client_secret))
+	picture = json.loads(h.request(url,'GET')[1])
 	
-	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-	#5. Grab the first image
-	#6. If no image is available, insert default a image url
+	url_picture = ''
+	if picture['response']['photos']['count'] != 0:
+		#Retrieve the first picture of the resturant
+		url_picture = picture['response']['photos']['items'][0]
+		url_picture = url_picture['prefix'] + "300x300" + url_picture['suffix']
+	else:
+		#If there is no picture, use a default picture.
+		url_picture = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Tom%27s_Restaurant%2C_NYC.jpg/1200px-Tom%27s_Restaurant%2C_NYC.jpg'
+	
+	return 
 	#7. Return a dictionary containing the restaurant name, address, and image url	
 if __name__ == '__main__':
-	findARestaurant("Pizza", "Tokyo, Japan")
+	#findARestaurant("Pizza", "Tokyo, Japan")
 	#findARestaurant("Tacos", "Jakarta, Indonesia")
 	#findARestaurant("Tapas", "Maputo, Mozambique")
 	#findARestaurant("Falafel", "Cairo, Egypt")
 	#findARestaurant("Spaghetti", "New Delhi, India")
 	#findARestaurant("Cappuccino", "Geneva, Switzerland")
-	#findARestaurant("Sushi", "Los Angeles, California")
+	findARestaurant("Sushi", "Los Angeles, California")
 	#findARestaurant("Steak", "La Paz, Bolivia")
 	#findARestaurant("Gyros", "Sydney Australia")

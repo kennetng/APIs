00f49f4e7fcced539945ce60a512154a2f3bbe47
Kenneth Ngo <>
2018-09-08 16:57:27 +0200

    DONE!@

--- making_API_Endpoint/Test3_BIG_PROJECT/views.py
+++ making_API_Endpoint/Test3_BIG_PROJECT/views.py
@@ -25,15 +25,52 @@ app = Flask(__name__)
 
 @app.route('/restaurants', methods = ['GET', 'POST'])
 def all_restaurants_handler():
-  #YOUR CODE HERE
+	#If GET, return all resturant in Database
+	if request.method == 'GET':
+		restaurants = session.query(Restaurant).all()
+		return jsonify(restaurants = (i.serialize for i in restaurants))
+	#If POST, make a new resturant and post it in database
+	elif request.method == 'POST':
+		location = request.args.get('location', '')
+		mealType = request.args.get('mealType', '')
+		restaurant_info = findARestaurant(mealType, location)
+
+		if restaurant_info != 'No Restaurants Found':
+			restaurant = Restaurant(restaurant_name = unicode(restaurant_info['name']), restaurant_address = unicode(restaurant_info['address']), restaurant_image = restaurant_info['image'])
+			session.add(restaurant)
+			session.commit()
+			return jsonify(restaurant = restaurant.serialize)
+		else:
+			return ({"error":"No Restaurants Found for %s in %s" % (mealType, location)})
+
     
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
-  #YOUR CODE HERE
+	restaurant = session.query(Restaurant).filter_by(id = id).one()
+	if request.method == 'GET':
+  		return jsonify(restaurant = restaurant.serialize)
+	elif request.method == 'PUT':
+  	#Update resturant
+  		address = request.args.get('address')
+  		image = request.args.get('image')
+  		name = request.args.get('name')
+  		if address:
+  			restaurant.restaurant_address = address
+  		if image:
+  			restaurant.restaurant_image = image
+  		if name:
+  			restaurant.restaurant_name = name
+	  	session.commit()
+	  	return jsonify(restaurant = restaurant.serialize)
+	elif request.method == 'DELETE':
+  		#Delete specified restaurant
+	  	session.delete(restaurant)
+	  	session.commit()
+	  	return "Restaurant Deleted"
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5004)
 
 
   
